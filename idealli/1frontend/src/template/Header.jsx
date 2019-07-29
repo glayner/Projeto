dbd8792dc962fefa14de34df505572b60ef68966
glayner <thiagoglayner_tg@hotmail.com>
2019-07-29 19:01:31 -0300

    idealli

--- idealli/1frontend/src/template/Header.jsx
+++
@@ -1,9 +0,0 @@
-import './Header.css'
-import React from 'react'
-import Dropdown from './DropdownProdutos'
-
-
-export default props =>
-    <header className='header '>
-        <Dropdown/>
-    </header>
